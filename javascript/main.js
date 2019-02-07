01e1b9d60ec647c104c8af0572ba2331a0b3dabb
Tasha <shradertasha@gmail.com>
2019-02-06 19:11:46 -0600

    <update>

--- javascript/main.js
+++ javascript/main.js
@@ -1,50 +1,54 @@
-$("button").on("click", function () {
+$(document).on("click", "#addDog", function () {
+    var dogType = $("#dogType").val().trim();
+    var newDog = $("<button>");
+    newDog.attr("data-animal", dogType);
+    newDog.html(dogType);
+    newDog.addClass("one");
+    $("#buttonLand").append(newDog);
+})
+
+
+
+$(".one").on("click", function() {
     event.preventDefault();
     var animal = $(this).attr("data-animal");
-    console.log("hello");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-
         .then(function (response) {
             var results = response.data;
+            for (var i = 0; i < results.length; i++) {
 
-            for (var i = 0; i < 9; i++) {
-                var rating = results[i].rating;
-                var p = ("Rating: ", rating);
+                var dogGif = $('<img>')
+                dogGif.attr('src', results[i].images.fixed_height_still.url);
+                dogGif.attr('data-still', results[i].images.fixed_height_still.url);
+                dogGif.attr('data-animate', results[i].images.fixed_height.url);
+                dogGif.addClass('gif');
+                dogGif.attr('data-state', "still");
+                $("#gifPlace").append(dogGif);
 
-                var stillImage = $("<img>");
-                stillImage.attr("src", results[i].images.fixed_height_still.url);
-                stillImage.addClass("still");
+                $(dogGif).on("click", function() {
+                    var state = $(this).attr('data-state');
+                    if (state == 'still') {
+                        $(this).attr('src', $(this).data('animate'));
+                        $(this).attr('data-state', 'animate');
+                    } else {
+                        $(this).attr('src', $(this).data('still'));
+                        $(this).attr('data-state', 'still');
+                   
+                    }
+                });
 
-                var animatedImage = $("<img>");
-                animatedImage.attr("src", results[i].images.fixed_height.url);
-                animatedImage.addClass('animated');
+               
 
-                $("#content").prepend(stillImage);
-                $("#raitings").prepend(p);
 
-            
 
-             
-
-                $(stillImage).on("click", function(){
-                    $("#still").attr("#animated");
-                    $(this).remove();
-                    $("#content").prepend(animatedImage);
-                })
-                $(animatedImage).on("click", function(){
-                    $("#still").attr("#still");
-                    $(this).remove();
-                    $("#content").prepend(stillImage);
-                })
-        
 
             }
 
         })
-         
-
 })
+
+
