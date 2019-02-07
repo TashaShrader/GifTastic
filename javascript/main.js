3f7682e78455494edc0eca805c89a972edc1b2c2
Tasha <shradertasha@gmail.com>
2019-02-07 16:41:48 -0600

    <update>

--- javascript/main.js
+++ javascript/main.js
@@ -9,7 +9,7 @@ $(document).on("click", "#addDog", function () {
 
 
 
-$(document).on("click", ".one", function() {
+$(document).on("click", ".one", function () {
     event.preventDefault();
     var animal = $(this).attr("data-animal");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -20,24 +20,19 @@ $(document).on("click", ".one", function() {
         .then(function (response) {
             var results = response.data;
             for (var i = 0; i < 9; i++) {
-              
-
-                // i have attempted this for several hours and im just not gettng it to display how i want it to :[
-               // var rating = $("<p>")
-               // rating.attr("src", results[i].rating);
-               // $("#gifPlace").append("Raiting: ", rating);
-
-
-                var dogGif = $('<img>')
+                
+                $("#gifPlace").append("<p>", "Rating: ", results[i].rating, "</p>");
+                var dogGif = $('<img>');
                 dogGif.attr('src', results[i].images.fixed_height_still.url);
                 dogGif.attr('data-still', results[i].images.fixed_height_still.url);
                 dogGif.attr('data-animate', results[i].images.fixed_height.url);
                 dogGif.addClass('gif');
+
                 dogGif.attr('data-state', "still");
-                dogGif.attr("width", "300px");
+
                 $("#gifPlace").append(dogGif);
 
-                $(dogGif).on("click", function() {
+                $(dogGif).on("click", function () {
                     var state = $(this).attr('data-state');
                     if (state == 'still') {
                         $(this).attr('src', $(this).data('animate'));
@@ -45,16 +40,16 @@ $(document).on("click", ".one", function() {
                     } else {
                         $(this).attr('src', $(this).data('still'));
                         $(this).attr('data-state', 'still');
-                   
+
                     }
-            
-               
+
+
                 })
-    
-           
+
+
 
 
             }
         })
-    
+
 })
